9820d9b99fbea5c570d7bdaf530a2c1def9325e5
Anders Bakken <agbakken@gmail.com>
2018-08-28 14:40:06 -0700

    mo better

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include <dlfcn.h>
+#include <string.h>
+#include <errno.h>
 #include <base.h>
 #include <stdio.h>
 
 int main()
 {
-    void *plug = dlopen("pluga/libpluga.so", RTLD_LOCAL);
+    void *plug = dlopen("pluga/libpluga.so", RTLD_LAZY|RTLD_LOCAL);
     if (!plug) {
         printf("[main.cpp:%d]: if (!plug) {\n", __LINE__); fflush(stdout);
+        printf("%s %s:%d\n", dlerror(), strerror(errno), errno);
         return 1;
     }
 
